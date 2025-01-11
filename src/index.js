30f91c17ffde2f21c36de806057b5599ea9226c3
Matias Raimondi <matiraimondi1@gmail.com>
2025-01-11 15:17:23 -0300

    Connect with OpenAI token.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv/config');
 const { Client, IntentsBitField } = require('discord.js');
+const { OpenAI } = require('openai');
 
 const client = new Client({
     intents: [
@@ -7,6 +8,10 @@ const client = new Client({
         IntentsBitField.Flags.GuildMessages,
         IntentsBitField.Flags.MessageContent,
     ]
+});
+
+const openai = new OpenAI({
+    apiKey: process.env.API_KEY,
 });
 
 client.on('ready', () => {
