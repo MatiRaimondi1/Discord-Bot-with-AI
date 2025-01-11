248f391f4c4541425d10ae54d76df023154019f3
Matias Raimondi <matiraimondi1@gmail.com>
2025-01-11 15:18:56 -0300

    Generate a response by sending a message.

--- src/index.js
+++ src/index.js
@@ -18,4 +18,24 @@ client.on('ready', () => {
     console.log("The bot is online.")
 });
 
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;
+
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'system',
+                content: "Chat GPT is a friendly chatbot."
+            },
+            {
+                role: 'user',
+                content: message.content,
+            }
+        ]
+    }).catch((error) => console.error('OpenAI error: ', error));
+
+    message.reply(response.choices[0].message.content);
+});
+
 client.login(process.env.TOKEN);
